425544f75338ce340aa7b3cab9b75ffdf5185374
dev.gyanendrapc <dev.gyanendrapc@gmail.com>
2022-08-17 07:11:20 +0545

    day-04 first assignment

---
+++ day-04/Assignment/EvenOdd/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+using static System.Convert;
+
+
+Write("Enter your number : ");
+var num = ToInt32(ReadLine());
+
+
+if(num % 2 == 0){
+    WriteLine($"{num} is even.");
+}else{
+    WriteLine($"{num} is odd.");
+}
---
+++ day-04/day-04.md
@@ -0,0 +1,4 @@
+1. Day-04
+    1. Single line comment //this is singleline comment and multiline comment /* this is multiline comment */
+    1. Squiggle error, warning, and info
+    1. Different DataTypes in CSharp
