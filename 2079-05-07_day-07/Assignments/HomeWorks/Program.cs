d670d4ac7563afbc3edef668b1f74d33d849ff5c
dev.gyanendrapc <dev.gyanendrapc@gmail.com>
2022-08-23 12:28:36 +0545

    Completed ClassWork And Assignments

--- 2079-05-07_day-07/Assignments/HomeWorks/Program.cs
+++ 2079-05-07_day-07/Assignments/HomeWorks/Program.cs
@@ -1,4 +1,7 @@
 ﻿Solutions sn = new();
+System.Console.WriteLine("Print Even");
 sn.PrintOdd();
+System.Console.WriteLine("Print Odd");
 sn.PrintEven();
+System.Console.WriteLine("Multiplication Table");
 sn.MulTable();
