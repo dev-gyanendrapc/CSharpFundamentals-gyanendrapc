db2f10a65f0b7b152af45a75bf687a9dddfd8ec0
dev.gyanendrapc <dev.gyanendrapc@gmail.com>
2022-08-25 11:05:50 +0545

    Notes

---
+++ 2079-05-09_day_09/HelloMethods/Program.cs
@@ -0,0 +1,13 @@
+﻿// Return something and take some arguments
+
+// double Add(double x, double y)
+// {
+//     return x + y;
+// }
+double Add(byte x, int y, float z)
+{
+    return x + y + z;
+}
+
+var sum = Add(20, 10, 10.5f);
+System.Console.WriteLine(sum);
